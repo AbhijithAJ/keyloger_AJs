e9b8b926b31cfebbcab3a579c80be66b48a85060
AbhijithAJ <47808835+AbhijithAJ@users.noreply.github.com>
2019-05-07 16:47:44 +0530

    Update kelg.py

--- kelg.py
+++ kelg.py
@@ -5,10 +5,7 @@ global data
 global count
 count = 0
 data = ''
-#log_dir = "" 
 
-# logging.basicConfig(filename=(log_dir + "log_k.txt"), level=logging.DEBUG,format='%(asctime)s :%(message)s') 
-# logging.basicConfig(filename=(log_dir + "log_k.txt"), level=logging.DEBUG,format='%(asctime)s :%(message)s') 
 previus_time = 0
 lst=[]
 
@@ -17,13 +14,10 @@ def save_data(data):
         f.write(data+'\n')
 
 def on_press(key):
-    #print(type(key))
     global lst
     if len(str(key)) > 3:
         a = str(key)+'pressed'
-        #print(type(a),a)
         lst.append(a)
-        #print('{0} pressed'.format(key)) 
 
 def on_release(key):                                              
         global data,previus_time,lst,count
@@ -34,7 +28,6 @@ def on_release(key):
                 a,b =key.split(' ')
                 if a==b:
                     key = a
-                #print('----',key)
                 count =1
                 lst=[]
         if count ==2:
@@ -43,17 +36,13 @@ def on_release(key):
     
         if len(str(key)) <=3 and len(str(key)) !=0:
                 data = data+str(key)[1]
-                print(data, key)
         elif len(str(key)) >3:
                 if 'spac' in str(key) and len(data) != 0:
                         data = data+' '
                 else:   
                         if (time.time()) > previus_time+settime:
-                                print('--------------------------')
-                                print('time:',(settime+time.time()),'previus_time',previus_time)
-                                data = str(datetime.datetime.now()) +'\n--------------------------\n'+str(data)
-                        data = str(data)+'\n__'+str(key)
-                        print('>>>>',data)
+                                data = '\n-------------------------\n'+str(datetime.datetime.now()) +'\n--------------------------\n'+str(data)
+                        data = str(data)+'\n'+str(key)
                         save_data(data)
                         if count==1:
                             count=2
